f6b455de71f97282e1b4fba04b59f3bd29bb248d
YLL <1067079944@qq.com>
2015-08-27 22:41:36 +0800

    Modify a.c

--- a.c
+++ a.c
@@ -6,12 +6,6 @@ int main(void)
     printf("我是于亮亮\n");
     printf("我是于亮亮\n");
     printf("我是于亮亮\n");
-    printf("我是于亮亮\n");
-    printf("我是于亮亮\n");
-    printf("我是于亮亮\n");
-    printf("我是于亮亮\n");
-    printf("我是于亮亮\n");
-    printf("我是于亮亮\n");
-    printf("我是于亮亮\n");
+    printf("shaodegui\n");
     return 0;
 }
